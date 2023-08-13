125be252f17dae8332a8cd3fae292fe4a7600132
Александр Ахтырский <armagidec88@gmail.com>
2023-08-13 17:08:24 +0300

    Subscription model

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,8 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   has_many :posts
+  has_many :publishers_relations, class_name: 'Subscription', foreign_key: :subscriber_id, dependent: :destroy
+  has_many :subscribers_relations, class_name: 'Subscription', foreign_key: :publisher_id, dependent: :destroy
+  has_many :subscribers, class_name: 'User', through: :subscribers_relations 
+  has_many :publishers, class_name: 'User', through: :publishers_relations
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_08_13_105556) do
+ActiveRecord::Schema[7.0].define(version: 2023_08_13_125133) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -18,6 +18,13 @@ ActiveRecord::Schema[7.0].define(version: 2023_08_13_105556) do
     t.integer "user_id"
     t.string "title"
     t.text "body"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "subscriptions", force: :cascade do |t|
+    t.integer "subscriber_id"
+    t.integer "publisher_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
