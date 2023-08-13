c21045de4d000247dcd7f0c8eae80b9ae6fe0fbc
Александр Ахтырский <armagidec88@gmail.com>
2023-08-13 18:42:28 +0300

    Seeds

--- app/models/subscription.rb
+++ app/models/subscription.rb
@@ -1,4 +1,4 @@
 class Subscription < ApplicationRecord
-  belongs_to: publisher, class_name: 'User', foreign_key: :publisher_id
-  belongs_to: subscriber, class_name: 'User', foreign_key: :subscriber_id
+  belongs_to :publisher, class_name: 'User', foreign_key: :publisher_id
+  belongs_to :subscriber, class_name: 'User', foreign_key: :subscriber_id
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+user_1 = User.create(username: 'First', email: "first@mail.ru", password: "somepassword") 
+user_2 = User.create(username: 'Second', email: "second@mail.ru", password: "somepassword")
+post = Post.create(user: user_1, body: "Some smart and intriguing text", title: "The Best Article Ever")
+Subscription.create(publisher: user_1, subscriber: user_2)
+comment = Comment.create(body: "Awesome!!!", subject: post, user: user_2, post_id: post.id)
+Comment.create(body: "Thanks!", subject: comment, user: user_1, post_id: post.id)
+
